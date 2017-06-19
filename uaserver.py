b2bc9a3e500f3e688d96d6ab6c44f156731e5451
lmillana <leyreeemcaballero@gmail.com>
2017-06-19 21:46:36 +0200

    Recibe el INVITE, pendiente de revisar metodo BYE

--- uaserver.py
+++ uaserver.py
@@ -20,6 +20,7 @@ class XMLHandler(ContentHandler):
 		self.list_dic = []
 
 	def startElement(self,name,attrs):
+		#Almancena en un dic los datos del XML:
 		if name == 'account':
 			self.tag_dic['username'] = attrs.get('username','--')
 			self.tag_dic['passwd'] = attrs.get('passwd','--')
@@ -69,155 +70,154 @@ class XMLHandler(ContentHandler):
 		return self.list_dic
 
 class ProxyHandler(socketserver.DatagramRequestHandler):
-    """
-    Proxy server class.
-    """
-    RTP = {'IP': '',
-           'PORT': []}
+	"""
+	Proxy server class.
+	"""
 
-    METHODS = ['INVITE', 'ACK', 'BYE']
+	RTP = {'IP': '',
+			'PORT': []}
 
-    def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address)
-        IP_CLIENT = str(self.client_address[0])
-        PORT_CLIENT = int(self.client_address[1])
+	METHODS = ['INVITE', 'ACK', 'BYE']
 
-        while 1:
-            # Leyendo línea a línea lo que nos envía el cliente
-            text = self.rfile.read()
-            LINE = line.decode('utf-8')
+	def handle(self):
+		# Escribe dirección y puerto del cliente (de tupla client_address)
+		IP_CLIENT = str(self.client_address[0])
+		PORT_CLIENT = int(self.client_address[1])
 
-            #Para salir del bucle (si no hay mas lineas):
-            if not LINE:
-            	break
+		while 1:
+			# Leyendo línea a línea lo que nos envía el cliente
+			text = self.rfile.read()
+			LINE = text.decode('utf-8')
 
-            method = text.decode('utf-8').split(' ')[0]
-            #print("Hemos recibido tu peticion:\r\n", LINE)
+			#Para salir del bucle (si no hay mas lineas):
+			if not LINE:
+				break
 
-            #RECEPTOR_IP = LINE[4].split(' ')[1].split('\r\n')[0]
-            #RECEPTOR_PORT = LINE[7].split(' ')[1].split(' ')[0]
+			method = text.decode('utf-8').split(' ')[0]
+			print('-----RECEIVED:\r\n' + LINE)
 
-            if not method in self.METHODS:
-                answer = 'SIP/2.0 405 Method Not Allowed\r\n\r\n'
-                #Enviamos el mensaje de respuesta:
-                self.wfile.write(bytes(answer, 'utf-8'))
-                #Añadimos al fichero LOG:
-                FICH_LOG(PATH_LOG, 'Error', IP_CLIENT, PORT_CLIENT, '')
+			IP_FROM = text.decode('utf-8').split()[8]
+			PORT_FROM = text.decode('utf-8').split()[-2]
 
-            elif method == 'INVITE':
-            	FICH_LOG(PATH_LOG,'Received from',RECEPTOR_IP,RECEPTOR_PORT,LINE)
+			if not method in self.METHODS:
+				answer = 'SIP/2.0 405 Method Not Allowed\r\n\r\n'
+				#Enviamos el mensaje de respuesta:
+				self.wfile.write(bytes(answer, 'utf-8'))
+				#Añadimos al fichero LOG:
+				FICH_LOG(PATH_LOG, 'Error', IP_CLIENT, PORT_CLIENT, '')
+
+			elif method == 'INVITE':
+				#Escribimos en el fichero LOG:
+				FICH_LOG(PATH_LOG,'Received from',IP_FROM, PORT_FROM,LINE)
                 
-                answer = 'SIP/2.0 100 Trying\r\n\r\n'
-                answer += 'SIP/2.0 180 Ring\r\n\r\n'
-                answer += 'SIP/2.0 200 OK\r\n\r\n'
-                #Añadimos las cabeceras: Header + Separator:
-                answer += 'Content-Type: application/sdp\r\n\r\n'
-                #Message Body:
-                answer += 'v=0\r\n' + 'o=' + USERNAME + ' ' + IP + '\r\n'
-                answer += 's=Prueba' + '\r\n' + 't=0' + '\r\n'
-                answer += 'm=audio' + PORT_AUDIO + 'RTP' + '\r\n'
+				answer = 'SIP/2.0 100 Trying\r\n\r\n'
+				answer += 'SIP/2.0 180 Ring\r\n\r\n'
+				answer += 'SIP/2.0 200 OK\r\n\r\n'
+				#Añadimos las cabeceras: Header + Separator:
+				answer += 'Content-Type: application/sdp\r\n\r\n'
+				#Message Body:
+				answer += 'v=0\r\n' + 'o=' + USERNAME + ' ' + IP + '\r\n'
+				answer += 's=Prueba' + '\r\n' + 't=0' + '\r\n'
+				answer += 'm=audio' + PORT_AUDIO + 'RTP' + '\r\n'
 
-                #Enviamos el mensaje de respuesta:
-                self.wfile.write(bytes(answer, 'utf-8'))
-                #Añadimos al diccionario RTP:
+				#Enviamos el mensaje de respuesta:
+				self.wfile.write(bytes(answer, 'utf-8'))
+				#Añadimos al diccionario RTP:
 
-                #INCOMPLETO!!!!
+				#Añadimos al fichero LOG:
+				FICH_LOG(PATH_LOG, 'Send to', IP_CLIENT, PORT_CLIENT, answer)
 
-                #Añadimos al fichero LOG:
-                response = answer.split('\r\n')
-                LINE = ' '.join(response)
-                FICH_LOG(PATH_LOG, 'Send to', IP_CLIENT, PORT_CLIENT, LINE)
+			elif method == 'ACK':
+				FICH_LOG(PATH_LOG, 'Received from', IP_FROM, PORT_FROM, answer)
 
-            elif method == 'ACK':
-            	FICH_LOG(PATH_LOG, 'Received from', RECEPTOR_IP, RECEPTOR_PORT, LINE)
+			#Envio RTP:
+			# aEjecutar es un string con lo que se ha de ejecutar en la shell
+				aEjecutar = './mp32rtp -i ' + self.RTP['IP']
+				aEjecutar += '-p' + self.RTP['PORT']
+				aEjecutar += '< ' + PATH_AUDIO
+				print ("Let's run ", aEjecutar)
+				os.system(aEjecutar)
+				#Añadimos al fichero LOG:
+				FICH_LOG(PATH_LOG, 'Send to', self.RTP['IP'], self.RTP['PORT'],'Audio File')
 
-            #Envio RTP:
-            # aEjecutar es un string con lo que se ha de ejecutar en la shell
-                aEjecutar = './mp32rtp -i ' + self.RTP['IP']
-                aEjecutar += '-p' + self.RTP['PORT']
-                aEjecutar += '< ' + PATH_AUDIO
-                print ("Let's run ", aEjecutar)
-                os.system(aEjecutar)
-                FICH_LOG(PATH_LOG, 'Send to', self.RTP['IP'], self.RTP['PORT'],'Audio File')
+				print('Finished transfer!')
+				FICH_LOG(PATH_LOG, 'Finished audio transfer','','','')
 
-                print('Finished transfer!')
-                FICH_LOG(PATH_LOG, 'Finished audio transfer','','','')
+			elif method == 'BYE':
+				FICH_LOG(PATH_LOG, 'Received from', IP_FROM, PORT_FROM, LINE)
 
-            elif method == 'BYE':
-            	FICH_LOG(PATH_LOG, 'Received from', RECEPTOR_IP, RECEPTOR_PORT, LINE)
+				answer = 'SIP/2.0 200 OK\r\n\r\n'
+				#Enviamos el mensaje de respuesta:
+				self.wfile.write(bytes(answer, 'utf-8'))
 
-                answer = 'SIP/2.0 200 OK\r\n\r\n'
-                #Enviamos el mensaje de respuesta:
-                self.wfile.write(bytes(answer, 'utf-8'))
-                #Añadimos al fichero LOG:
-                response = answer.split('\r\n')
-                LINE = ' '.join(response)
-                FICH_LOG(PATH_LOG, 'Send to', IP_CLIENT, PORT_CLIENT, LINE)
+				#Añadimos al fichero LOG:
+				FICH_LOG(PATH_LOG, 'Send to', IP_CLIENT, PORT_CLIENT, answer)
 
-            else:
-            	FICH_LOG(PATH_LOG, 'Received from', RECEPTOR_IP, RECEPTOR_PORT, LINE)
 
-                answer = 'SIP/2.0 400 Bad Request\r\n\r\n'
-                #Enviamos el mensaje de respuesta:
-                self.wfile.write(bytes(answer, 'utf-8'))
-                #Añadimos al fichero LOG:
-                response = answer.split('\r\n')
-                LINE = ' '.join(response)
-                FICH_LOG(PATH_LOG, 'Error', IP_CLIENT, PORT_CLIENT, '')
+			else:
+				FICH_LOG(PATH_LOG, 'Received from', RECEPTOR_IP, RECEPTOR_PORT, LINE)
+
+				answer = 'SIP/2.0 400 Bad Request\r\n\r\n'
+				#Enviamos el mensaje de respuesta:
+				self.wfile.write(bytes(answer, 'utf-8'))
+				#Añadimos al fichero LOG:
+				response = answer.split('\r\n')
+				#LINE = ' '.join(response)
+				FICH_LOG(PATH_LOG, 'Error', IP_CLIENT, PORT_CLIENT, '')
 
 
 if __name__ == "__main__":
 
-    if len(sys.argv) != 2:
-        sys.exit("Usage: python3 uaserver.py config")
+	if len(sys.argv) != 2:
+		sys.exit("Usage: python3 uaserver.py config")
 
-    try:
-        CONFIG = sys.argv[1]
+	try:
+		CONFIG = sys.argv[1]
 
-        if not os.path.exists(CONFIG):
-            print("File doesn't exist!")
-            sys.exit("Usage: python3 uaserver.py config")
+		if not os.path.exists(CONFIG):
+			print("File doesn't exist!")
+			sys.exit("Usage: python3 uaserver.py config")
 
-    except IndexError:
-        sys.exit("Usage: python3 uaserver.py config")
+	except IndexError:
+		sys.exit("Usage: python3 uaserver.py config")
 
-    #Abrimos el fichero XML
-    fich = open(CONFIG, 'r')
-    line = fich.readlines()
-    fich.close()
+	#Abrimos el fichero XML
+	fich = open(CONFIG, 'r')
+	line = fich.readlines()
+	fich.close()
 
-    #USERNAME + PASSWORD:
-    USERNAME = line[4].split(">")[1].split("<")[0]
-    PASSWORD = line[5].split(">")[1].split("<")[0]
+	#USERNAME + PASSWORD:
+	USERNAME = line[4].split(">")[1].split("<")[0]
+	PASSWORD = line[5].split(">")[1].split("<")[0]
 
-    #UASERVER, IP + PUERTO:
-    IP = line[8].split(">")[1].split("<")[0]
-    PORT = line[9].split(">")[1].split("<")[0]
+	#UASERVER, IP + PUERTO:
+	IP = line[8].split(">")[1].split("<")[0]
+	PORT = line[9].split(">")[1].split("<")[0]
 
-    #RTP AUDIO:
-    PORT_AUDIO = line[12].split(">")[1].split("<")[0]
+	#RTP AUDIO:
+	PORT_AUDIO = line[12].split(">")[1].split("<")[0]
 
-    #PROXY, IP + PUERTO:
-    IP_PROXY = line[15].split(">")[1].split("<")[0]
-    PORT_PROXY = line[16].split(">")[1].split("<")[0]
+	#PROXY, IP + PUERTO:
+	IP_PROXY = line[15].split(">")[1].split("<")[0]
+	PORT_PROXY = line[16].split(">")[1].split("<")[0]
 
-    #LOG(localización del fichero de log):
-    PATH_LOG = line[19].split(">")[1].split("<")[0]
+	#LOG(localización del fichero de log):
+	PATH_LOG = line[19].split(">")[1].split("<")[0]
 
-    #AUDIO, PATH(localizacion del ficherod el audio):
-    PATH_AUDIO = line[22].split(">")[1].split("<")[0]
+	#AUDIO, PATH(localizacion del ficherod el audio):
+	PATH_AUDIO = line[22].split(">")[1].split("<")[0]
 
-    # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((IP_PROXY, int(PORT_PROXY)))
+	# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+	my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	my_socket.connect((IP_PROXY, int(PORT_PROXY)))
 
-    # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer((IP, int(PORT)), ProxyHandler)
-    print("Listening...")
+	# Creamos servidor de eco y escuchamos
+	serv = socketserver.UDPServer((IP, int(PORT)), ProxyHandler)
+	print("Listening...")
 
-    try:
-        serv.serve_forever()
-    except KeyboardInterrupt:
-        FICH_LOG(PATH_LOG, 'Error', IP_PROXY, PORT_PROXY, '')
-        sys.exit('\r\nEnded server')
+	try:
+		serv.serve_forever()
+	except KeyboardInterrupt:
+		FICH_LOG(PATH_LOG, 'Error', IP_PROXY, PORT_PROXY, '')
+		sys.exit('\r\nEnded server')
